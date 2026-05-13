54bd34563f1e02016c4dc1e57888059548bc305a
jacobfriisstrand <jacob@friis.com>
2026-05-13 11:35:00 +0200

    feat(slice-2): share menu block between header and footer linklists

--- src/schemas/blocks/menu.schema.ts
+++ src/schemas/blocks/menu.schema.ts
@@ -3,4 +3,13 @@ import { defineBlock } from "../types.ts";
 export default defineBlock({
   name: "t:names.menu",
   tag: null,
+  settings: [
+    {
+      type: "link_list",
+      id: "menu",
+      label: "t:labels.menu",
+      default: "footer",
+    },
+  ],
+  presets: [{ name: "t:names.menu" }],
 });
