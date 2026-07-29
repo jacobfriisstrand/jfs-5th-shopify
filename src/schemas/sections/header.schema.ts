96bda2d2c882b1eb84529ba2b321eb75ff8d2cf4
jacobfriisstrand <jacob@friis.com>
2026-07-29 13:15:22 +0200

    chore(navigation-module): update locales, schemas, templates

--- src/schemas/sections/header.schema.ts
+++ src/schemas/sections/header.schema.ts
@@ -3,4 +3,11 @@ import { defineSection } from "../types.ts";
 export default defineSection({
   name: "t:names.header",
   tag: "header",
+  blocks: [
+    { type: "_header-logo" },
+    { type: "_header-menu" },
+    { type: "_header-search" },
+    { type: "_header-account" },
+    { type: "_header-cart" },
+  ],
 });
