5a11317ad4b3d1b360bd5eac5aa27c2ad841b747
Jacob Friis Strand <jacob@friis.com>
2026-07-31 09:11:25 +0200

    feat(vertical-image-block): add vertical-image-block section with dynamic header height (#89)
    * feat(vertical-image-block): add vertical-image-block section with image block
    
    * fix(vertical-image-block): dynamic header height via ResizeObserver
    
    * style(vertical-image-block): layout spacing and component polish

--- src/schemas/blocks/image.schema.ts
+++ src/schemas/blocks/image.schema.ts
@@ -3,4 +3,16 @@ import { defineBlock } from "../types.ts";
 export default defineBlock({
   name: "t:names.image",
   tag: null,
+  settings: [
+    {
+      type: "image_picker",
+      id: "image",
+      label: "t:settings.image",
+    },
+  ],
+  presets: [
+    {
+      name: "t:names.image",
+    },
+  ],
 });
