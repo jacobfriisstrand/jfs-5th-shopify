6ae4bc06b24052f7d81211f40c78df776f57b16a
jacobfriisstrand <jacob@friis.com>
2026-08-06 09:17:05 +0200

    feat(footer): menu block headings removed, legal links, show_legal_links

--- src/schemas/blocks/menu.schema.ts
+++ src/schemas/blocks/menu.schema.ts
@@ -5,15 +5,16 @@ export default defineBlock({
   tag: null,
   settings: [
     {
-      type: "text",
-      id: "heading",
-      label: "t:content.heading",
-    },
-    {
       type: "link_list",
       id: "menu",
       label: "t:content.menu",
       default: "main-menu",
+    },
+    {
+      type: "checkbox",
+      id: "show_legal_links",
+      label: "t:content.show_legal_links",
+      default: false,
     },
   ],
   presets: [{ name: "t:names.menu" }],
